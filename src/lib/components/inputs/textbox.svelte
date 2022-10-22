551f93d58543a466d4150a1f26618eb34f5ee097
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-10-22 14:00:52 +0100

    added isPassword option to button component

--- src/lib/components/inputs/textbox.svelte
+++ src/lib/components/inputs/textbox.svelte
@@ -2,16 +2,27 @@
 	export let placeholderText: string = '';
 	export let labelText: string = '';
 	export let textboxContent: string = '';
+	export let isPassword: boolean = false;
 </script>
 
 <div class="flex flex-col-reverse gap-1">
-	<input
-		type="text"
-		id="componentTextbox"
-		class="bg-gray-100 border border-gray-300 text-sm rounded-lg block w-full p-2.5"
-		placeholder={placeholderText}
-		bind:value={textboxContent}
-	/>
+	{#if isPassword}
+		<input
+			type="password"
+			id="componentTextbox"
+			class="bg-gray-100 border border-gray-300 text-sm rounded-lg block w-full p-2.5"
+			placeholder={placeholderText}
+			bind:value={textboxContent}
+		/>
+	{:else}
+		<input
+			type="text"
+			id="componentTextbox"
+			class="bg-gray-100 border border-gray-300 text-sm rounded-lg block w-full p-2.5"
+			placeholder={placeholderText}
+			bind:value={textboxContent}
+		/>
+	{/if}
 	{#if labelText !== ''}
 		<label for="componentTextbox" class="text-sm">{labelText}</label>
 	{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
 	import { Textbox, Listbox } from '$lib/components/inputs';
-	import { Button } from '$lib/components/button';
 </script>
 
 <div class="m-12">
-	<Listbox labelText="Select child" placeholderOption="Please select child">
-		<option value="A">Alex</option>
-		<option value="O">Oliver</option>
-		<option value="J">James</option>
-	</Listbox>
-	<br />
-	<Textbox labelText="First name" placeholderText="Jon" />
+	<Textbox isPassword={true} labelText="Password" placeholderText="Password" />
 </div>
