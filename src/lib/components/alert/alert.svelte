a2376013d3db9ca840a58d79682f683903a89b78
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-10-25 17:40:19 +0100

    Update alert.svelte

--- src/lib/components/alert/alert.svelte
+++ src/lib/components/alert/alert.svelte
@@ -17,13 +17,16 @@
 	}
 </script>
 
-<div class="bg-gray-100 p-4 m-4 rounded-3xl">
+<div class="bg-gray-100 p-4 m-4 rounded-2xl max-w-2xl">
 	<h1 class="font-bold text-2xl">{title}</h1>
 	<p class="opacity-50 leading-5">{body}</p>
-	<div class="flex justify-between mt-4">
+	<!-- <div class="flex justify-between mt-4 "> -->
+	<div class="flex flex-col-reverse items-center gap-2 mt-4 md:flex-row md:justify-between">
 		{#if secondaryButtonText !== ''}
 			<Button on:click={secondaryButtonDispatcher} style="secondary">{secondaryButtonText}</Button>
 		{/if}
-		<Button on:click={primaryButtonDispatcher} style="primary">{buttonText}</Button>
+		<div class="w-64">
+			<Button on:click={primaryButtonDispatcher} style="primary">{buttonText}</Button>
+		</div>
 	</div>
 </div>
