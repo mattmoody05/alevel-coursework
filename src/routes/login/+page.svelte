3180159d741855da9391c48c42d198ce20839809
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-10-25 17:42:02 +0100

    updated login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { Textbox } from '$lib/components/input';
 	import { Button, LinkButton } from '$lib/components/button';
+	import { Alert } from '$lib/components/alert';
+	import { goto } from '$app/navigation';
+
+	let showIncorrectPasswordPopup: boolean = false;
+
+	function checkCredentials() {
+		showIncorrectPasswordPopup = true;
+	}
 </script>
 
 <svelte:head>
@@ -12,9 +20,10 @@
 		<div class="h-full flex justify-center flex-col">
 			<div class="flex flex-col gap-2 m-8">
 				<h1 class="font-bold text-3xl">Login</h1>
+				<p class="opacity-50">Please enter your username and password to login to the system.</p>
 				<Textbox labelText="Username" placeholderText="jdoe" />
 				<Textbox labelText="Password" isPassword={true} />
-				<Button>Login</Button>
+				<Button on:click={checkCredentials}>Login</Button>
 				<LinkButton href="/register" style="secondary"
 					>Don't have an account? Register here</LinkButton
 				>
@@ -23,3 +32,18 @@
 		<div class="w-full h-full bg-gradient-to-br from-blue-500 to-blue-200 hidden md:block" />
 	</div>
 </main>
+
+{#if showIncorrectPasswordPopup}
+	<div
+		class="absolute top-0 left-0 h-screen w-screen bg-black bg-opacity-80 flex justify-center items-center"
+	>
+		<Alert
+			title="❗Incorrect username or password"
+			body="The username or password that you have input is incorrect. If you have forgotten your password, reset it using the link below."
+			buttonText="Acknowledge"
+			secondaryButtonText="Forgot your password?"
+			on:secondary-click={() => goto('/forgot-password')}
+			on:primary-click={() => (showIncorrectPasswordPopup = false)}
+		/>
+	</div>
+{/if}
