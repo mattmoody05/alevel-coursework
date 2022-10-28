779912d9e5d4338fdcc622782aece2885728ae09
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-10-28 14:55:48 +0100

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,11 +4,28 @@
 	import { Alert } from '$lib/components/alert';
 	import { goto } from '$app/navigation';
 
+	import { presenceCheck } from '$lib/util/validation';
+	import { onMount } from 'svelte';
+
+	let username: string = '';
+	let password: string = '';
+	let loggedIn = false;
+
 	let showIncorrectPasswordPopup: boolean = false;
 
 	function checkCredentials() {
-		showIncorrectPasswordPopup = true;
+		if ((presenceCheck(username) && presenceCheck(password)) === false) {
+			showIncorrectPasswordPopup = true;
+		} else {
+			localStorage.setItem('username', username);
+		}
 	}
+
+	onMount(() => {
+		if (localStorage.getItem('username')) {
+			loggedIn = true;
+		}
+	});
 </script>
 
 <svelte:head>
@@ -19,14 +36,31 @@
 	<div class="grid grid-cols-1 md:grid-cols-2 h-screen">
 		<div class="h-full flex justify-center flex-col">
 			<div class="flex flex-col gap-2 m-8">
-				<h1 class="font-bold text-3xl">Login</h1>
-				<p class="opacity-50">Please enter your username and password to login to the system.</p>
-				<Textbox labelText="Username" placeholderText="jdoe" />
-				<Textbox labelText="Password" isPassword={true} />
-				<Button on:click={checkCredentials}>Login</Button>
-				<LinkButton href="/register" style="secondary"
-					>Don't have an account? Register here</LinkButton
-				>
+				{#if !loggedIn}
+					<h1 class="font-bold text-3xl">Login</h1>
+					<p class="opacity-50">Please enter your username and password to login to the system.</p>
+					<Textbox labelText="Username" placeholderText="jdoe" bind:value={username} />
+					<Textbox labelText="Password" isPassword={true} bind:value={password} />
+					<Button on:click={checkCredentials}>Login</Button>
+					<LinkButton href="/register" style="secondary">
+						Don't have an account? Register here
+					</LinkButton>
+				{:else}
+					<h1 class="font-bold text-3xl">👋 Welcome back {localStorage.getItem('username')}</h1>
+					<div class="max-w-min">
+						<Button on:click={() => (loggedIn = false)} style="secondary"
+							>Not {localStorage.getItem('username')}? Login here</Button
+						>
+					</div>
+					<div class="max-w-min">
+						<LinkButton href="/" style="primary">
+							<div class="flex gap-2 items-center">
+								<span>Dashboard</span>
+								<i class="fa-solid fa-arrow-right" />
+							</div>
+						</LinkButton>
+					</div>
+				{/if}
 			</div>
 		</div>
 		<div class="w-full h-full bg-gradient-to-br from-blue-500 to-blue-200 hidden md:block" />
@@ -39,7 +73,7 @@
 	>
 		<Alert
 			title="❗Incorrect username or password"
-			body="The username or password that you have input is incorrect. If you have forgotten your password, reset it using the link below."
+			body="The username or password that you have input is incorrect, or a field has been left blank. If you have forgotten your password, reset it using the link below."
 			buttonText="Acknowledge"
 			secondaryButtonText="Forgot your password?"
 			on:secondary-click={() => goto('/forgot-password')}
