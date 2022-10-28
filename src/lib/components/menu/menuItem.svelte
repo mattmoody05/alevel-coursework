cc12f8b275dd1cbd78b38154d13bcca542ff6953
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-10-28 14:55:13 +0100

    Create menuItem.svelte

---
+++ src/lib/components/menu/menuItem.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let selected: boolean = false;
+	export let iconClass: string = 'fa-solid fa-gauge';
+	export let itemName: string = 'Dashboard';
+
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function onClickDispatcher() {
+		dispatch('click');
+	}
+</script>
+
+<button
+	on:click={onClickDispatcher}
+	class="flex gap-2 items-center p-3 hover:cursor-pointer rounded-xl w-64 {selected
+		? 'bg-gray-900 hover:bg-gray-700 text-white font-bold'
+		: 'hover:bg-gray-200'}"
+>
+	<div class="w-4 flex items-center justify-center"><i class={iconClass} /></div>
+	<p>{itemName}</p>
+</button>
