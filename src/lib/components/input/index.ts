abd859dadfdae72a522ecb055d2697623b931776
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-10-30 11:04:34 +0000

    updated input components

--- src/lib/components/input/index.ts
+++ src/lib/components/input/index.ts
@@ -1,4 +1,5 @@
-export { default as Textbox } from "./textbox.svelte"
-export { default as Listbox } from "./listbox.svelte"
-export { default as NumericUpDown } from "./numericUpDown.svelte"
-export { default as LargeTextbox } from "./largeTextbox.svelte"
+export { default as Textbox } from "./textbox.svelte";
+export { default as Listbox } from "./listbox.svelte";
+export { default as NumericUpDown } from "./numericUpDown.svelte";
+export { default as LargeTextbox } from "./largeTextbox.svelte";
+export { default as ValidationMessage } from "./validationMessage.svelte";
