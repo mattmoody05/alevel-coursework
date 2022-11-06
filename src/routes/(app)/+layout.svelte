ee035e8b36473c9ea0c36bd4036a1bc5b52b9edf
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-06 19:32:17 +0000

    updated layout

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,10 +1,25 @@
-<script>
+<script lang="ts">
+	import Header from '$lib/components/header/header.svelte';
 	import { Menu } from '$lib/components/menu';
+	import { page } from '$app/stores';
+	import { afterNavigate } from '$app/navigation';
+
+	let selectedRouteUrl: string = $page.url.pathname;
+	afterNavigate(() => {
+		selectedRouteUrl = `/${$page.url.pathname.split('/')[1]}`;
+	});
 </script>
 
 <div class="flex p-4 w-screen h-screen">
 	<Menu />
 	<div class="ml-4 w-full overflow-y-scroll">
-		<slot />
+		{#if selectedRouteUrl !== '/'}
+			<Header />
+			<div class="mt-4">
+				<slot />
+			</div>
+		{:else}
+			<slot />
+		{/if}
 	</div>
 </div>
