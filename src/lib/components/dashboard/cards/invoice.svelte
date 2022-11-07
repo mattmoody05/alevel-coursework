24102323e03056df8726d18174623f6ff2625d6a
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-07 10:45:19 +0000

    added invoice card

---
+++ src/lib/components/dashboard/cards/invoice.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { CardWrapper } from '$lib/components/dashboard/cards';
+	import type { invoiceSummary } from '$lib/util/types';
+	export let recentInvoices: invoiceSummary[];
+	export let invoicesIssued: number;
+	export let invoicesDue: number;
+
+	const currencyFormatter: Intl.NumberFormat = new Intl.NumberFormat('en-GB', {
+		style: 'currency',
+		currency: 'GBP'
+	});
+</script>
+
+<CardWrapper title="Invoices">
+	<div class="w-full grid grid-cols-2">
+		<div class="flex flex-col">
+			<span class="text-sm opacity-50">Invoices issued</span>
+			<span class="text-4xl font-bold">{String(invoicesIssued)}</span>
+		</div>
+		<div class="flex flex-col">
+			<span class="text-sm opacity-50">Invoices due</span>
+			<span class="text-4xl font-bold">{String(invoicesDue)}</span>
+		</div>
+	</div>
+	<div class="mt-4">
+		<span>Recent invoices</span>
+		<div class="flex flex-col gap-2 mt-1 opacity-50">
+			{#each recentInvoices as invoice}
+				<div class="grid grid-cols-2 text-sm items-center">
+					<span>{currencyFormatter.format(invoice.amountDue / 100)}</span>
+					<span class="text-center">Due {invoice.dueDate}</span>
+				</div>
+			{/each}
+		</div>
+	</div>
+</CardWrapper>
