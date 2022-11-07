e982ac1285937321be9ff3184d70de323cb8dab5
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-07 10:50:55 +0000

    created absence card

---
+++ src/lib/components/dashboard/cards/absence.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { CardWrapper } from '$lib/components/dashboard/cards';
+	import type { absentReportSummary } from '$lib/util/types';
+	export let absencesReported: number;
+	export let absenceReports: absentReportSummary[];
+</script>
+
+<CardWrapper title="Absences">
+	<div class="flex flex-col">
+		<span class="text-sm opacity-50">Absences reported</span>
+		<span class="text-4xl font-bold">{String(absencesReported)}</span>
+	</div>
+	<div class="mt-4">
+		<span>Absence reports</span>
+		<div class="flex flex-col gap-2 mt-1">
+			{#each absenceReports as absence}
+				<div class="grid grid-cols-2 text-sm items-center">
+					<div class="bg-violet-500 text-white rounded-lg px-2 py-1 max-w-max">
+						{absence.childName}
+					</div>
+					<span class="text-center opacity-50">{absence.startDate} - {absence.endDate}</span>
+				</div>
+			{/each}
+		</div>
+	</div>
+</CardWrapper>
