df1d0b4d580c58ca70477e7d8ce5fed2da0719cc
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-07 10:51:12 +0000

    Update index.ts

--- src/lib/components/dashboard/cards/index.ts
+++ src/lib/components/dashboard/cards/index.ts
@@ -1,3 +1,6 @@
-export { default as ThisWeekCard } from "./thisWeek.svelte";
-export { default as UrgentNotificationCard } from "./urgentNotification.svelte";
-export { default as CardWrapper } from "./wrapper.svelte"
+export { default as ThisWeekCard } from './thisWeek.svelte';
+export { default as UrgentNotificationCard } from './urgentNotification.svelte';
+export { default as CardWrapper } from './wrapper.svelte';
+export { default as AbsenceCard } from './absence.svelte';
+export { default as SurveyCard } from './survey.svelte';
+export { default as InvoiceCard } from './invoice.svelte';
