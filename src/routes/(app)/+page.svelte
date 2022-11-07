e8edc270b68a53982d221e1fe075d6dec3a40c9e
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-07 14:47:54 +0000

    dashboard mockup added

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,1 +1,122 @@
-dashboard
+<script lang="ts">
+	import {
+		AbsenceCard,
+		InvoiceCard,
+		SurveyCard,
+		ThisWeekCard,
+		UrgentNotificationCard
+	} from '$lib/components/dashboard/cards';
+	import { getCustomDateString } from '$lib/util/date';
+	import { onMount } from 'svelte';
+	let dateString: string = getCustomDateString();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			dateString = getCustomDateString();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<svelte:head>
+	<title>Dashboard</title>
+</svelte:head>
+
+<div class="header flex flex-col lg:flex-row lg:justify-between lg:items-center mb-4">
+	<h1 class="font-bold text-3xl">Welcome back, Matthew</h1>
+	<h3 class="opacity-50 text-xl font-medium">{dateString}</h3>
+</div>
+<div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 mt-2">
+	<ThisWeekCard
+		childrenBooked={10}
+		sessionsBooked={3}
+		recentSessions={[
+			{
+				childName: 'Matthew',
+				date: '10/12/22',
+				time: '11:00'
+			},
+			{
+				childName: 'Ella',
+				date: '08/12/22',
+				time: '09:00'
+			},
+			{
+				childName: 'Kabir',
+				date: '05/12/22',
+				time: '11:00'
+			},
+			{
+				childName: 'Dan',
+				date: '05/12/22',
+				time: '09:00'
+			}
+		]}
+	/>
+	<UrgentNotificationCard
+		recentNotification={{
+			notificationName: 'Coronavirus regulations',
+			content:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+		}}
+		totalUrgentNotifications={10}
+		unreadUrgentNotifications={1}
+	/>
+	<AbsenceCard
+		absenceReports={[
+			{
+				childName: 'Jon',
+				startDate: '10/11/22',
+				endDate: '12/11/22'
+			}
+		]}
+		absencesReported={1}
+	/>
+	<SurveyCard
+		surveysIssued={10}
+		surveysPending={2}
+		recentSurveys={[
+			{
+				dueDate: '20/10/22',
+				surveyName: 'Photo consent form'
+			},
+			{
+				dueDate: '01/10/22',
+				surveyName: 'Monthly check in'
+			},
+			{
+				dueDate: '01/09/22',
+				surveyName: 'Monthly check in'
+			},
+			{
+				dueDate: '01/09/22',
+				surveyName: 'Back to school survey'
+			}
+		]}
+	/>
+	<InvoiceCard
+		invoicesDue={1}
+		invoicesIssued={10}
+		recentInvoices={[
+			{
+				amountDue: 11889,
+				dueDate: '01/11/22'
+			},
+			{
+				amountDue: 7910,
+				dueDate: '01/10/22'
+			},
+			{
+				amountDue: 10967,
+				dueDate: '01/09/22'
+			},
+			{
+				amountDue: 5779,
+				dueDate: '01/08/22'
+			}
+		]}
+	/>
+</div>
