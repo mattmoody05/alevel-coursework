45ca7866eb8f7d1de5f0a4656b1218bec937ea8a
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-12 11:59:14 +0000

    added two way message component

---
+++ src/lib/components/messages/index.ts
@@ -0,0 +1,1 @@
+export { default as TwoWayMessage } from '$lib/components/messages/twoWayMessage.svelte';
---
+++ src/lib/components/messages/twoWayMessage.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	type twoWayMessageType = 'incoming' | 'outgoing';
+	export let messageStyle: twoWayMessageType;
+</script>
+
+{#if messageStyle === 'incoming'}
+	<div class="flex justify-start mt-4 ">
+		<div class="max-w-[75%] p-2.5 rounded-xl bg-gray-100 border border-gray-300">
+			<slot />
+		</div>
+	</div>
+{:else if messageStyle === 'outgoing'}
+	<div class="flex justify-end mt-4">
+		<div
+			class="max-w-[75%] from-indigo-700 to-indigo-500 bg-gradient-to-tl text-white p-2.5 rounded-xl"
+		>
+			<slot />
+		</div>
+	</div>
+{/if}
