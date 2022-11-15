cef03bcc93b0eda765163db23370a8e3817db37a
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-15 21:31:53 +0000

    updated placeholder absence main menu page

--- src/routes/(app)/absence/+page.svelte
+++ src/routes/(app)/absence/+page.svelte
@@ -1,1 +1,8 @@
-absence main menu
+<script>
+	import { LinkButton } from '$lib/components/button';
+</script>
+
+<div class="flex flex-col gap-4">
+	<LinkButton style="secondary" href="/absence/report">Report an absence</LinkButton>
+	<LinkButton style="secondary" href="/absence/view">View absences</LinkButton>
+</div>
