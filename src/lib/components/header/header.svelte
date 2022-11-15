64c3bcbc26636061b256dd8ca9232eae0e74fa9b
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-15 21:30:29 +0000

    updated back button and date formatting in header

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -19,14 +19,16 @@
 	class="flex justify-between p-2 rounded-xl border border-gray-300 bg-gray-100 overflow-y-scroll min-h-max "
 >
 	<button
-		class="text-sm hover:bg-gray-200 max-w-max p-2 rounded-lg hover:cursor-pointer"
+		class="text-sm hover:bg-gray-200 max-w-max p-2 rounded-lg hover:cursor-pointer flex gap-1 items-center"
 		on:click={() => history.back()}
 	>
 		<i class="fa-solid fa-arrow-left opacity-50" />
-		<span class="opacity-50 ml-1">Back</span>
+		<span class="opacity-50">Back</span>
 	</button>
 
-	<div class="backButton text-sm hover:bg-gray-200 max-w-max p-2 rounded-lg hover:cursor-pointer">
+	<div
+		class="backButton text-sm hover:bg-gray-200 max-w-max p-2 rounded-lg hover:cursor-pointer flex items-center"
+	>
 		<span class="opacity-50">{dateString}</span>
 	</div>
 </div>
