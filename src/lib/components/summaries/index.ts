8f634e0590cd1c889decf01f9638a018d1f1a5a1
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-22 13:06:06 +0000

    added session summary

--- src/lib/components/summaries/index.ts
+++ src/lib/components/summaries/index.ts
@@ -1,2 +1,3 @@
 export { default as AbsenceSummary } from '$lib/components/summaries/absence.svelte';
 export { default as ExpenseSummary } from '$lib/components/summaries/expense.svelte';
+export { default as SessionSummary } from '$lib/components/summaries/session.svelte';
---
+++ src/lib/components/summaries/session.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { stringToColour } from '$lib/util/ui';
+
+	export let childName: string = 'Matthew';
+	export let date: string = '10/11/12';
+	export let time: string = '11:00';
+	export let length: number = 120;
+	export let sessionId: string = 'idHere';
+</script>
+
+<a href="/session/view/{sessionId}">
+	<div class="grid grid-cols-3 lg:grid-cols-5 p-2 bg-gray-100 border-gray-300 border rounded-xl">
+		<div>
+			<div class="px-2 py-1 {stringToColour(childName)} text-white rounded-lg max-w-max mr-2">
+				{childName}
+			</div>
+		</div>
+		<div class="lg:col-span-2 lg:grid lg:grid-cols-2 flex">
+			<span class="flex items-center">{date}</span>
+			<span class="lg:hidden flex items-center">@</span>
+			<span class="flex items-center">{time}</span>
+		</div>
+		<div class="flex items-center">{length / 60} hours</div>
+		<div class="hidden lg:flex justify-end">
+			<a href="/session/view/{sessionId}">
+				<div
+					class="bg-gray-900 text-white text-sm font-bold rounded-lg hover:bg-gray-700 py-1 px-2 min-w-max w-32 max-w-full flex items-center justify-center h-full"
+				>
+					View
+				</div>
+			</a>
+		</div>
+	</div>
+</a>
