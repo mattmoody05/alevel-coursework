5f6a0f1539471639a62a283e7e17f745619c4222
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-25 12:21:20 +0000

    added time off summary

--- src/lib/components/summaries/index.ts
+++ src/lib/components/summaries/index.ts
@@ -1,3 +1,4 @@
 export { default as AbsenceSummary } from '$lib/components/summaries/absence.svelte';
 export { default as ExpenseSummary } from '$lib/components/summaries/expense.svelte';
 export { default as SessionSummary } from '$lib/components/summaries/session.svelte';
+export { default as TimeOffSummary } from '$lib/components/summaries/timeOff.svelte';
---
+++ src/lib/components/summaries/timeOff.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<a href="/time-off/view/idHere">
+	<div class="grid grid-cols-3 lg:grid-cols-4 border bg-gray-100 border-gray-300 rounded-xl p-2">
+		<div class="flex items-center">10/11/22</div>
+		<div class="flex items-center">12/11/22</div>
+		<div class="flex items-center">Yes</div>
+		<div class="hidden lg:flex justify-end">
+			<a href="/time-off/view/idHere">
+				<div
+					class="bg-gray-900 text-white text-sm font-bold rounded-lg hover:bg-gray-700 p-2 min-w-max w-32 max-w-full flex items-center justify-center h-full"
+				>
+					View
+				</div>
+			</a>
+		</div>
+	</div>
+</a>
