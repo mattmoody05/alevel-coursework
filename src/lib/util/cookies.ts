e45eaf086e2bf4a42fe4768bc92c6a534bb589e7
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-29 16:11:06 +0000

    Update cookies.ts

--- src/lib/util/cookies.ts
+++ src/lib/util/cookies.ts
@@ -6,7 +6,7 @@ export function removeCookie(cookieName: string) {
 	document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
 
-function getCookie(cookieName: string) {
+function getCookie(cookieName: string): string {
 	const name = `${cookieName}=`;
 	const decodedCookie = decodeURIComponent(document.cookie);
 	const ca = decodedCookie.split(';');
