c82ec23f0e71991ee8ab24970b63e8990a9cb40e
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-29 13:11:22 +0000

    cookie util functions

---
+++ src/lib/util/cookies.ts
@@ -0,0 +1,23 @@
+export function setCookie(cookieName: string, cookieValue: string) {
+	document.cookie = `${cookieName}=${cookieValue}; path=/`;
+}
+
+export function removeCookie(cookieName: string) {
+	document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
+
+function getCookie(cookieName: string) {
+	const name = `${cookieName}=`;
+	const decodedCookie = decodeURIComponent(document.cookie);
+	const ca = decodedCookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return '';
+}
