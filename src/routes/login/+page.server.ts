155ab55743396aacc6745f24b7f038c7f50d9631
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-29 17:03:07 +0000

    updated login page to use sveltekit actions

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,60 @@
+import type { account } from '$lib/util/types';
+import { error, redirect } from '@sveltejs/kit';
+import { openDb } from '../../db/index';
+import type { Actions, PageServerLoad, PageServerLoadEvent, RequestEvent } from './$types';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const load: PageServerLoad = async ({ cookies }: PageServerLoadEvent) => {
+	const token = cookies.get('token');
+	if (token !== undefined) {
+		// Check that the token is valid and that the user exists
+		throw redirect(300, '/');
+	}
+};
+
+export const actions: Actions = {
+	login: async ({ request, cookies }: RequestEvent) => {
+		console.log('login function run');
+
+		const data = await request.formData();
+
+		// @ts-ignore
+		const username: string = data.get('username');
+		// @ts-ignore
+		const password: string = data.get('password');
+		const db = await openDb();
+
+		if (username !== null && password !== null) {
+			const reqAccount: account | undefined = await db.get(
+				`SELECT * FROM account WHERE username = ?`,
+				username
+			);
+
+			if (reqAccount !== undefined) {
+				const passwordsMatch = await bcrypt.compare(password, reqAccount.password);
+
+				if (passwordsMatch) {
+					const token = jwt.sign(
+						{ accountId: reqAccount.accountId },
+						process.env['JWT_SIGNING_SECRET_KEY']
+					);
+
+					cookies.set('token', token);
+					throw redirect(300, '/');
+				}
+
+				// Password does not matched the one stored in DB
+				throw error(400, 'incorrect password');
+			}
+
+			// User not found with username specified not found
+			throw error(400, 'user not found with username specified');
+		}
+		// username or password is null
+		throw error(400, 'username or password is null');
+	}
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,32 +1,6 @@
 <script lang="ts">
 	import { Textbox } from '$lib/components/input';
 	import { Button, LinkButton } from '$lib/components/button';
-	import { Alert } from '$lib/components/alert';
-	import { goto } from '$app/navigation';
-
-	import { presenceCheck } from '$lib/util/validation';
-	import { onMount } from 'svelte';
-
-	let username: string = '';
-	let password: string = '';
-	let loggedIn = false;
-
-	let showIncorrectPasswordPopup: boolean = false;
-
-	function checkCredentials() {
-		if ((presenceCheck(username) && presenceCheck(password)) === false) {
-			showIncorrectPasswordPopup = true;
-		} else {
-			localStorage.setItem('username', username);
-			goto('/');
-		}
-	}
-
-	onMount(() => {
-		if (localStorage.getItem('username')) {
-			loggedIn = true;
-		}
-	});
 </script>
 
 <svelte:head>
@@ -37,48 +11,18 @@
 	<div class="grid grid-cols-1 md:grid-cols-2 h-screen">
 		<div class="h-full flex justify-center flex-col">
 			<div class="flex flex-col gap-2 m-8">
-				{#if !loggedIn}
-					<h1 class="font-bold text-3xl">Login</h1>
-					<p class="opacity-50">Please enter your username and password to login to the system.</p>
-					<Textbox labelText="Username" placeholderText="jdoe" bind:value={username} />
-					<Textbox labelText="Password" isPassword={true} bind:value={password} />
-					<Button on:click={checkCredentials}>Login</Button>
-					<LinkButton href="/register" style="secondary">
-						Don't have an account? Register here
-					</LinkButton>
-				{:else}
-					<h1 class="font-bold text-3xl">👋 Welcome back {localStorage.getItem('username')}</h1>
-					<div class="max-w-min">
-						<Button on:click={() => (loggedIn = false)} style="secondary"
-							>Not {localStorage.getItem('username')}? Login here</Button
-						>
-					</div>
-					<div class="max-w-min">
-						<LinkButton href="/" style="primary">
-							<div class="flex gap-2 items-center">
-								<span>Dashboard</span>
-								<i class="fa-solid fa-arrow-right" />
-							</div>
-						</LinkButton>
-					</div>
-				{/if}
+				<h1 class="font-bold text-3xl">Login</h1>
+				<p class="opacity-50">Please enter your username and password to login to the system.</p>
+				<form class="flex flex-col gap-2" action="?/login" method="post" id="loginForm">
+					<Textbox name="username" labelText="Username" placeholderText="jdoe" />
+					<Textbox name="password" labelText="Password" isPassword={true} />
+					<Button style="submit">Login</Button>
+				</form>
+				<LinkButton href="/register" style="secondary">
+					Don't have an account? Register here
+				</LinkButton>
 			</div>
 		</div>
 		<div class="w-full h-full bg-gradient-to-br from-blue-500 to-blue-200 hidden md:block" />
 	</div>
 </main>
-
-{#if showIncorrectPasswordPopup}
-	<div
-		class="absolute top-0 left-0 h-screen w-screen bg-black bg-opacity-80 flex justify-center items-center"
-	>
-		<Alert
-			title="❗Incorrect username or password"
-			body="The username or password that you have input is incorrect, or a field has been left blank. If you have forgotten your password, reset it using the link below."
-			buttonText="Acknowledge"
-			secondaryButtonText="Forgot your password?"
-			on:secondary-click={() => goto('/forgot-password')}
-			on:primary-click={() => (showIncorrectPasswordPopup = false)}
-		/>
-	</div>
-{/if}
