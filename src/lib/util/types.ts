cca12b8ae4ddf57b5984ea45767ac608d347ad9f
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-11-29 13:11:11 +0000

    added types for database tables

--- src/lib/util/types.ts
+++ src/lib/util/types.ts
@@ -41,3 +41,156 @@ export type parentSummary = {
 	id: string;
 	name: string;
 };
+
+export type account = {
+	accountId: string;
+	username: string;
+	password: string;
+	isAdmins: boolean;
+	parentId?: string;
+};
+
+export type parent = {
+	parentId: string;
+	firstName: string;
+	lastName: string;
+	phoneNumber: string;
+	emailAddress: string;
+	dateOfBirth: string;
+	houseNumber: string;
+	postcode: string;
+	accountId: string;
+};
+
+export type session = {
+	sessionId: string;
+	date: string;
+	startTime: string;
+	length: number;
+	dateBooked: string;
+	absent: boolean;
+	absenceCharge: boolean;
+	isRecurring: boolean;
+	childId: string;
+	invoiceId?: string;
+};
+
+export type expense = {
+	expenseId: string;
+	name: string;
+	cost: number;
+	date: string;
+	type: string;
+	supportingDocs: string;
+	chargeToParents: boolean;
+	dateRecorded: string;
+	invoiceId?: string;
+};
+
+export type recurringSessionRequest = {
+	recurringSessionId: string;
+	approved: boolean;
+	recurringBasis: string;
+	mondaySelected: boolean;
+	tuesdaySelected: boolean;
+	wednesdaySelected: boolean;
+	thursdaySelected: boolean;
+	fridaySelected: boolean;
+	startTime: string;
+	endTime: string;
+	dateRequestSubmitted: string;
+	dateApproved: string;
+	childId: string;
+};
+
+export type survey = {
+	surveyId: string;
+	title: string;
+	description?: string;
+	consentForm: boolean;
+	anonymous: boolean;
+	numberOfQuestions: number;
+	dateCreated: string;
+};
+
+export type surveyResponse = {
+	surveyResponseId: string;
+	dateRecorded: string;
+	parentId: string;
+	surveyId: string;
+	surveyQuestionId: string;
+	surveyQuestionOptionId: string;
+};
+
+export type surveyIssue = {
+	surveyIssueId: string;
+	dateIssued: string;
+	surveyId: string;
+	parentId: string;
+};
+
+export type surveyQuestion = {
+	surveyQuestionId: string;
+	prompt: string;
+	dateCreated: string;
+	surveyId: string;
+};
+
+export type surveyQuestionOption = {
+	surveyQuestionOptionId: string;
+	prompt: string;
+	dateCreated: string;
+	surveyQuestionId: string;
+};
+
+export type shortNoticeNotifcation = {
+	notificationId: string;
+	message: string;
+	dateCreated: string;
+};
+
+export type shortNoticeNotifcationIssue = {
+	notificationIssueId: string;
+	allParents: boolean;
+	dateIssued: string;
+	parentId: string;
+	notificationId: string;
+};
+
+export type twoWayMessage = {
+	messageId: string;
+	messageContent: string;
+	fromOwner: boolean;
+	dateSent: string;
+	parentId: string;
+};
+
+export type child = {
+	childId: string;
+	firstName: string;
+	lastName: string;
+	dateOfBirth: string;
+	educationName?: string;
+	educationType: string;
+	additionalNeedDesc?: string;
+	additionalNeedType: string;
+	parentId: string;
+};
+
+export type invoice = {
+	invoiceId: string;
+	dateGenerated: string;
+	dateIssued: string;
+	dateDue: string;
+	startDate: string;
+	endDate: string;
+	includeExpenses: boolean;
+	additionalChargeName?: string;
+	additionalChargeAmount?: number;
+	discountName?: string;
+	discountAmount?: number;
+	message?: string;
+	total: number;
+	parentId: string;
+	childId: string;
+};
