abaffa0de1b10b366b82de38606d1a17e39f87a6
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2022-12-07 18:06:30 +0000

    removed dedicated invoice issue page
    all the inputs are now included on the page used to generate the invoice

--- src/routes/(app)/invoicing/+page.svelte
+++ src/routes/(app)/invoicing/+page.svelte
@@ -6,6 +6,5 @@
 
 <div class="flex flex-col gap-4">
 	<LinkButton style="secondary" href="/invoicing/create">Create invoice</LinkButton>
-	<LinkButton style="secondary" href="/invoicing/issue">Issue invoice</LinkButton>
 	<LinkButton style="secondary" href="/invoicing/view">View invoices</LinkButton>
 </div>
