bd6e6799ce83bbf58e37e4f45864a29f3695d125
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-01-07 18:32:18 +0000

    added admin check to view survey results

--- src/routes/(app)/survey/view/+page.server.ts
+++ src/routes/(app)/survey/view/+page.server.ts
@@ -2,10 +2,14 @@ import { getAllSurveys } from '$lib/util/db';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad, PageServerLoadEvent } from './$types';
 
-export const load: PageServerLoad = async ({}: PageServerLoadEvent) => {
-	const surveys = await getAllSurveys();
-	if (surveys !== undefined) {
-		return { surveys };
+export const load: PageServerLoad = async ({ locals }: PageServerLoadEvent) => {
+	const { isAdmin } = locals;
+	if (isAdmin) {
+		const surveys = await getAllSurveys();
+		if (surveys !== undefined) {
+			return { surveys };
+		}
+		throw error(500, 'surveys undefined');
 	}
-	throw error(500, 'surveys undefined');
+	throw error(400, 'must be admin');
 };
