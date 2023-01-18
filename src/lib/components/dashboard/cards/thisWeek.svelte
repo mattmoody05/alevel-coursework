7917fa99f7d601c6dcf6b09f87fb07d26691b335
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-01-18 19:31:24 +0000

    thisWeek card updates
    concatinates the recentSessions array to the four most recent elements
    states in card how many elements are not shown

--- src/lib/components/dashboard/cards/thisWeek.svelte
+++ src/lib/components/dashboard/cards/thisWeek.svelte
@@ -6,6 +6,19 @@
 	export let recentSessions: sessionSummary[];
 	export let sessionsBooked: number;
 	export let childrenBooked: number;
+
+	let visibleRecentSessions: sessionSummary[] = [];
+	let sessionOverflowAmount: number = 0;
+
+	if (recentSessions.length > 4) {
+		for (let i = recentSessions.length - 1; i >= recentSessions.length - 4; i--) {
+			const currentSession = recentSessions[i];
+			visibleRecentSessions = [...visibleRecentSessions, currentSession];
+		}
+		sessionOverflowAmount = recentSessions.length - visibleRecentSessions.length;
+	} else {
+		visibleRecentSessions = recentSessions;
+	}
 </script>
 
 <CardWrapper title="This week">
@@ -22,7 +35,7 @@
 	<div class="mt-4">
 		<span>Sessions</span>
 		<div class="flex flex-col gap-2 mt-1">
-			{#each recentSessions as session}
+			{#each visibleRecentSessions as session}
 				<div class="grid grid-cols-3 text-sm items-center">
 					<div
 						class="{stringToColour(session.childName)} text-white rounded-lg px-2 py-1 max-w-max"
@@ -35,4 +48,9 @@
 			{/each}
 		</div>
 	</div>
+	{#if sessionOverflowAmount > 0}
+		<div class="w-full flex justify-center mt-4">
+			<span class="opacity-50 text-sm">{sessionOverflowAmount} more hidden</span>
+		</div>
+	{/if}
 </CardWrapper>
