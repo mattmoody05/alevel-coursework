f4dddf0e5848b7df693657232c89d62cb9b1f3f6
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-01-19 21:57:42 +0000

    shoudl've been on last commit :(

--- src/routes/(app)/invoicing/view/+page.svelte
+++ src/routes/(app)/invoicing/view/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import { FilterButton, SortButton } from '$lib/components/filters';
 	import { InvoiceSummary } from '$lib/components/summaries';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	function getChildName(childId: string) {
+		for (let i = 0; i < data.children.length; i++) {
+			const currentChild = data.children[i];
+			if (currentChild.childId === childId) {
+				return currentChild.firstName;
+			}
+		}
+		return '';
+	}
 </script>
 
 <svelte:head>
@@ -9,8 +22,8 @@
 
 <h3 class="font-bold text-xl">View invoices</h3>
 <div class="flex gap-2 my-2">
-	<FilterButton />
-	<SortButton />
+	<!-- <FilterButton />
+	<SortButton /> -->
 </div>
 <div class="grid text-sm opacity-50 grid-cols-3 lg:grid-cols-6 my-2">
 	<span>Child</span>
@@ -19,4 +32,13 @@
 	<span class="">Date due</span>
 	<span>Payment status</span>
 </div>
-<InvoiceSummary />
+{#each data.invoices as invoice}
+	<InvoiceSummary
+		invoiceId={invoice.invoiceId}
+		childName={getChildName(invoice.childId)}
+		dateDue={invoice.dateDue}
+		dateIssued={invoice.dateIssued}
+		payable={invoice.total}
+		status={invoice.paymentStatus}
+	/>
+{/each}
