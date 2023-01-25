4dbe27505b03edd3f0a18bbd82c1f3698fb17eb5
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-01-25 19:43:40 +0000

    refactored dashboard +page.server.ts

--- src/routes/(app)/(dashboard)/+page.server.ts
+++ src/routes/(app)/(dashboard)/+page.server.ts
@@ -1,6 +1,5 @@
 import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad, PageServerLoadEvent } from './$types';
-import type { child, parent, session, shortNoticeNotifcation } from '$lib/util/types';
 import {
 	getChildren,
 	getParent,
@@ -12,56 +11,58 @@ import {
 } from '$lib/util/db';
 
 export const load: PageServerLoad = async ({ locals }: PageServerLoadEvent) => {
-	const account = locals.account;
-	const isAdmin = locals.isAdmin;
+	const { account, isAdmin } = locals;
 
 	if (account !== undefined) {
-		// admin stuff
-		if (isAdmin) {
-			const sessions = await getAllSessions();
-			if (sessions !== undefined) {
-				const children = await getAllChildren();
-				if (children !== undefined) {
-					const notifications = await getAllShortNoticeNotifications();
-					if (notifications !== undefined) {
-						return {
-							sessions,
-							children,
-							notifications,
-							isAdmin
-						};
-					}
-					throw error(500, 'notifications undefined');
-				}
-				throw error(500, 'children not defined');
-			}
-			throw error(500, 'sessions not defined');
-		}
+		if (isAdmin === true) {
+			// An admin is the currently logged in user
+			// Data in realation to all parents is fetched from the database
 
-		// parent account stuff
-		const parentData: parent | undefined = await getParent(account.accountId, 'account');
-		if (parentData !== undefined) {
-			const sessions: session[] | undefined = await getAllParentSessions(parentData.parentId);
-			if (sessions !== undefined) {
-				const children: child[] | undefined = await getChildren(parentData.parentId);
-				if (children !== undefined) {
-					const notifications = await getShortNoticeNotifications(parentData.parentId);
-					if (notifications !== undefined) {
-						return {
-							parentData,
-							sessions,
-							children,
-							notifications,
-							isAdmin
-						};
-					}
-					throw error(500, 'notifications undefined');
-				}
-				throw error(500, 'children undefined');
+			// Session, child, notification data is fetched from the database
+			const sessions = await getAllSessions();
+			const children = await getAllChildren();
+			const notifications = await getAllShortNoticeNotifications();
+
+			// Data is returned so that it can be used as part of the HTML template
+			return {
+				sessions,
+				children,
+				notifications,
+				isAdmin
+			};
+		} else {
+			// A parent is the currently logged in user
+			// Data in realation to the current parent only is fetched from the database
+
+			// Parent data is fetched from the database using the current user's accountId
+			const parentData = await getParent(account.accountId, 'account');
+			if (parentData !== undefined) {
+				// Session, child, notification data is fetched from the database using the current parentId
+				const sessions = await getAllParentSessions(parentData.parentId);
+				const children = await getChildren(parentData.parentId);
+				const notifications = await getShortNoticeNotifications(parentData.parentId);
+
+				// Data is returned so that it can be used as part of the HTML template
+				return {
+					parentData,
+					sessions,
+					children,
+					notifications,
+					isAdmin
+				};
+			} else {
+				// No parent was found in the database with a matching parentId
+				// 500: Internal server error code
+				throw error(
+					500,
+					"Could not get the data for the parent with the current user's accountId from the database. If an admin account is being used, please switch to a parent account."
+				);
 			}
-			throw error(400, 'sessions undefined');
 		}
-		throw error(400, 'parentData undefined');
+	} else {
+		// No user is currently logged in
+		// User is redirected to the login page
+		// 308: Permanent redirect code
+		throw redirect(308, '/login');
 	}
-	throw redirect(307, '/login');
 };
