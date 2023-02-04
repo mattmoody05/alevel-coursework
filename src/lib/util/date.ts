32c1485211e7a1326fd51f8a83f8bdfd1a521be7
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-02-04 15:24:52 +0000

    added function to date util file

--- src/lib/util/date.ts
+++ src/lib/util/date.ts
@@ -18,3 +18,19 @@ export function getDateFromLocaleString(localeString: string): Date {
 	const date = new Date(Number(splitString[2]), Number(splitString[1]) - 1, Number(splitString[0]));
 	return date;
 }
+
+export function differenceBetweenTimes(startTime: string, endTime: string) {
+	let minsDifference: number = 0;
+
+	let startTimeHours: number = Number(startTime.split(':')[0]);
+	const startTimeMins: number = Number(startTime.split(':')[1]);
+	const endTimeHours: number = Number(endTime.split(':')[0]);
+	const endTimeMins: number = Number(endTime.split(':')[1]);
+
+	minsDifference = minsDifference + endTimeMins;
+	minsDifference = minsDifference + 60 - startTimeMins;
+	startTimeHours = startTimeHours + 1;
+	minsDifference = minsDifference + (endTimeHours - startTimeHours) * 60;
+
+	return minsDifference;
+}
