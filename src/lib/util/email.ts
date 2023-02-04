dab68cf092117ae6993b9e75944ab43df1067fa5
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-02-04 15:24:25 +0000

    added Mailer class

---
+++ src/lib/util/email.ts
@@ -0,0 +1,31 @@
+import {
+	NODEMAILER_HOST,
+	NODEMAILER_PASSWORD,
+	NODEMAILER_PORT,
+	NODEMAILER_USERNAME
+} from '$env/static/private';
+import { createTransport } from 'nodemailer';
+
+export class Mailer {
+	#email: string;
+
+	constructor(email: string) {
+		this.#email = email;
+	}
+
+	sendEmail(options: { subject: string; body: string }) {
+		const transporter = createTransport({
+			host: NODEMAILER_HOST,
+			port: Number(NODEMAILER_PORT),
+			auth: {
+				user: NODEMAILER_USERNAME,
+				pass: NODEMAILER_PASSWORD
+			}
+		});
+		transporter.sendMail({
+			to: this.#email,
+			subject: options.subject,
+			text: options.body
+		});
+	}
+}
