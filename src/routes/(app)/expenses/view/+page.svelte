008ca80f7afc478a64a2d59059efcadabae47b1c
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-03-06 16:43:44 +0000

    added filtering to viewing expenses

--- src/routes/(app)/expenses/view/+page.svelte
+++ src/routes/(app)/expenses/view/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import { SmallAlert } from '$lib/components/alert';
-	import { FilterButton, SortButton } from '$lib/components/filters';
+	import { Button } from '$lib/components/button';
+	import { Checkbox } from '$lib/components/checkbox';
+	import { Listbox, Textbox } from '$lib/components/input';
 	import { ExpenseSummary } from '$lib/components/summaries';
+	import { getDateFromLocaleString } from '$lib/util/date';
+	import type { ExpenseTable } from '$lib/util/newDb';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import type { PageData } from './$types';
@@ -16,6 +20,105 @@
 			}, 5000);
 		}
 	});
+
+	let sortDate: boolean = false;
+	let filterType: string = 'default';
+	let filterDate: string;
+	let filterChargeStatus = 'default';
+
+	let visibleExpenses = data.expenses;
+
+	function updateFilters() {
+		let working: ExpenseTable[] = data.expenses;
+		if (sortDate === true) {
+			function quickSort(arr: ExpenseTable[]): ExpenseTable[] {
+				if (arr.length <= 1) {
+					// Base case: an array of length 1 is already sorted
+					return arr;
+				}
+
+				function getDateInt(expense: ExpenseTable) {
+					return getDateFromLocaleString(expense.date).getTime();
+				}
+
+				// Choose a pivot element
+				const pivotIndex = Math.floor(arr.length / 2);
+				const pivot = arr[pivotIndex];
+
+				// Partition the array around the pivot
+				let less: ExpenseTable[] = [];
+				let greater: ExpenseTable[] = [];
+				for (let i = 0; i < arr.length; i++) {
+					if (i === pivotIndex) {
+						// Skip the pivot element
+						continue;
+					}
+					if (getDateInt(arr[i]) < getDateInt(pivot)) {
+						less = [...less, arr[i]];
+					} else {
+						greater = [...greater, arr[i]];
+					}
+				}
+
+				// Recursively sort the two partitions
+				const sortedLess = quickSort(less);
+				const sortedGreater = quickSort(greater);
+
+				// Combine the sorted partitions with the pivot element
+				return [...sortedLess, pivot, ...sortedGreater];
+			}
+
+			working = quickSort(working);
+		}
+
+		if (filterDate !== '') {
+			let innerWorking: ExpenseTable[] = [];
+			for (let i = 0; i < working.length; i++) {
+				const currentExpense = working[i];
+				if (currentExpense.date === filterDate) {
+					innerWorking = [...innerWorking, currentExpense];
+				}
+			}
+			working = innerWorking;
+		}
+
+		if (filterType !== 'default') {
+			let innerWorking: ExpenseTable[] = [];
+			for (let i = 0; i < working.length; i++) {
+				const currentExpense = working[i];
+				if (currentExpense.type === filterType) {
+					innerWorking = [...innerWorking, currentExpense];
+				}
+			}
+			working = innerWorking;
+		}
+
+		if (filterChargeStatus === 'charge') {
+			let innerWorking: ExpenseTable[] = [];
+			for (let i = 0; i < working.length; i++) {
+				const currentExpense = working[i];
+				if (currentExpense.chargeToParents == true) {
+					innerWorking = [...innerWorking, currentExpense];
+				}
+			}
+			working = innerWorking;
+		} else if (filterChargeStatus === 'no-charge') {
+			let innerWorking: ExpenseTable[] = [];
+			for (let i = 0; i < working.length; i++) {
+				const currentExpense = working[i];
+				if (currentExpense.chargeToParents == false) {
+					innerWorking = [...innerWorking, currentExpense];
+				}
+			}
+			working = innerWorking;
+		}
+		visibleExpenses = working;
+	}
+
+	let showFilters: boolean = true;
+	function toggleShowFilters() {
+		showFilters = !showFilters;
+	}
 </script>
 
 <svelte:head>
@@ -35,10 +138,30 @@
 {/if}
 
 <h3 class="font-bold text-xl">View expenses</h3>
-<div class="flex gap-2">
-	<!-- <FilterButton />
-	<SortButton /> -->
+<div class="py-4">
+	<button on:click={toggleShowFilters} class="opacity-50 text-sm">
+		<i class="fa-solid fa-arrow-{showFilters ? 'up' : 'down'}" />
+		{showFilters ? 'Hide' : 'Show'} filters and sorts
+	</button>
 </div>
+{#if showFilters === true}
+	<div class="flex flex-col gap-2">
+		<div class="grid grid-cols-4 items-center gap-4">
+			<Textbox placeholderText="DD/MM/YYYY" labelText="Date filter" bind:value={filterDate} />
+			<Checkbox labelText="Sort by date" bind:isChecked={sortDate} />
+			<Listbox bind:value={filterType} labelText="Expense type">
+				<option value="Fuel">Fuel</option>
+				<option value="Food">Food</option>
+				<option value="Activity">Activity</option>
+			</Listbox>
+			<Listbox bind:value={filterChargeStatus} labelText="Charge to parents">
+				<option value="charge">Charge to parents</option>
+				<option value="no-charge">Do not charge to parents</option>
+			</Listbox>
+		</div>
+		<Button on:click={updateFilters}>Update results</Button>
+	</div>
+{/if}
 <div class="grid grid-cols-3 lg:grid-cols-6 p-2 rounded-xl text-sm opacity-50">
 	<span>Date</span>
 	<span>Name</span>
@@ -48,7 +171,7 @@
 </div>
 
 <div class="flex flex-col gap-2">
-	{#each data.expenses as expense}
+	{#each visibleExpenses as expense}
 		<ExpenseSummary
 			id={expense.expenseId}
 			charge={expense.chargeToParents}
