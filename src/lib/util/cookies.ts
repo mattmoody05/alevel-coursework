67566ba11ad36967569f4612362453a5e3c63c66
Matthew Moody <34244370+mattmoody05@users.noreply.github.com>
2023-03-13 14:13:08 +0000

    more

--- src/lib/util/cookies.ts
+++ src/lib/util/cookies.ts
@@ -1,6 +1,9 @@
 import type { Cookies } from '@sveltejs/kit';
 import jwt from 'jsonwebtoken';
 
+// If specified, fetches the accountId from the request
+// The accountId is housed in a JSON Web Token which is stored inside cookies
+// Cookies are sent alongside every request to the server
 export function getAccountId(cookiesObj: Cookies): string | undefined {
 	const token: string | undefined = cookiesObj.get('token');
 	if (token !== undefined) {
--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -4,5 +4,6 @@ export const load: PageServerLoad = async ({ cookies }: PageServerLoadEvent) => 
 	// Deletes the cookie that holds the user's accountId
 	cookies.delete('token');
 
+	// Returns data so that it can be used in the HTML template
 	return { success: true };
 };
